c872cffa31230d42d142083682f670843414a370
cassiorozelli <40122249+cassiorozelli@users.noreply.github.com>
2026-02-03 10:46:37 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const sqlite3
